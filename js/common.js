da88b67897f9005a29457654078117aca6ba677b
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-05-20 00:24:50 +0300

    c

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
 // parallax
 
 function parallax(e) {
-	let parallaxOffset = e.offsetX;
+	let parallaxOffset = e.clientX;
 	let layers = this.querySelectorAll('.scene-layer');
 	layers.forEach(item => {
 		let layerSpeed = item.getAttribute('data-number');
@@ -13,13 +13,39 @@ function parallax(e) {
 
 document.addEventListener('mousemove', parallax )
 
-let headerButton = document.querySelector('.header-menu-button')
-headerButton.addEventListener('click', function(e) {
-	this.classList.toggle('is-active');
-});
 
-document.addEventListener('click', function(e) {
-	if (e.target !== headerButton) {
-		headerButton.classList.remove('is-active')
-	}
-})
+
+function openSiderbar () {
+
+	let headerButton = document.querySelector('.header-menu-button');
+	let marchMenu = document.querySelector('.march-menu');
+	let MenuLink = marchMenu.querySelectorAll('.march-menu__link');
+	let body = document.querySelector('.site-body');
+	let siteContent = document.getElementById('site-content');
+	headerButton.addEventListener('click', function(e) {
+		this.classList.toggle('is-active');
+		marchMenu.classList.toggle('is-active');
+		body.classList.toggle('is-active');
+	});
+
+	document.addEventListener('click', function(e) {
+		if (e.target == siteContent) {
+			headerButton.classList.remove('is-active')
+			marchMenu.classList.remove('is-active');
+			body.classList.remove('is-active');
+		}
+
+		
+		MenuLink.forEach(link => {
+			link.addEventListener('click', function(e) {
+				if (e.target == this) {
+					headerButton.classList.remove('is-active')
+					marchMenu.classList.remove('is-active');
+					body.classList.remove('is-active');
+				}
+			})
+		});
+	})
+}
+
+openSiderbar ()
