3db46656fcf8b4d950606188b061adba1e9f3eb8
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-05-19 14:00:00 +0300

    c

--- js/common.js
+++ js/common.js
@@ -12,3 +12,14 @@ function parallax(e) {
 }
 
 document.addEventListener('mousemove', parallax )
+
+let headerButton = document.querySelector('.header-menu-button')
+headerButton.addEventListener('click', function(e) {
+	this.classList.toggle('is-active');
+});
+
+document.addEventListener('click', function(e) {
+	if (e.target !== headerButton) {
+		headerButton.classList.remove('is-active')
+	}
+})
